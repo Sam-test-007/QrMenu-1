b51decd280540fe0e616d0410eed4eb0e206544e
Samarpan-77 <samarpanaryal7+github@gmail.com>
2025-09-15 21:35:38 +0545

    Export project to GitHub

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,11 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
+import 'dotenv/config';
+
+
+const port = process.env.PORT || 5000;
+
 
 const app = express();
 app.use(express.json());
@@ -63,8 +68,8 @@ app.use((req, res, next) => {
   const port = parseInt(process.env.PORT || '5000', 10);
   server.listen({
     port,
-    host: "0.0.0.0",
-    reusePort: true,
+    host: "127.0.0.1",
+  
   }, () => {
     log(`serving on port ${port}`);
   });
